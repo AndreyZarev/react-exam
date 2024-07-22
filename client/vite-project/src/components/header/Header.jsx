c2c439f7303726f2153d2e7a88f68b918ce97146
tocode <andrey.zarev@outlook.com>
2024-07-22 18:04:00 +0300

    Create Header.jsx

---
+++ client/vite-project/src/components/header/Header.jsx
@@ -0,0 +1,91 @@
+
+import { NavLink } from "react-router-dom"
+const baseUrl = "http://localhost:3030/jsonstore"
+
+export default function Header() {
+
+    return (
+        <>
+            <header>
+                <div className="hero_area">
+                    <header className="header_section">
+                        <div className="container-fluid">
+                            <nav className="navbar navbar-expand-lg custom_nav-container ">
+                                <a className="navbar-brand" href="index.html">
+                                    <span>Neogym</span>
+                                </a>
+                                <button
+                                    className="navbar-toggler"
+                                    type="button"
+                                    data-toggle="collapse"
+                                    data-target="#navbarSupportedContent"
+                                    aria-controls="navbarSupportedContent"
+                                    aria-expanded="false"
+                                    aria-label="Toggle navigation"
+                                >
+                                    <span className="navbar-toggler-icon"></span>
+                                </button>
+                                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                    <div className="d-flex ml-auto flex-column flex-lg-row align-items-center">
+                                        <ul className="navbar-nav  ">
+                                            <li className="nav-item active">
+
+                                                <NavLink className="nav-link"
+                                                    style={
+                                                        ({ isActive }) => isActive ? { "border-bottom": "solid red 3px" } : {}
+                                                    }
+                                                    to="/">
+                                                    Home
+                                                </NavLink>
+                                            </li>
+                                            <li className="nav-item ">
+                                                <NavLink className="nav-link"
+                                                    style={
+                                                        ({ isActive }) => isActive ? { "border-bottom": "solid red 3px" } : {}
+                                                    }
+                                                    to="/why-us">
+                                                    Why Us
+                                                </NavLink>
+                                            </li>
+                                            <li className="nav-item">
+                                                <NavLink className="nav-link"
+                                                    style={
+                                                        ({ isActive }) => isActive ? { "borderBottom": "solid red 3px" } : {}
+                                                    }
+                                                    to="/trainers">
+                                                    Trainers
+                                                </NavLink>
+                                            </li>
+                                            <li className="nav-item">
+                                                <NavLink className="nav-link"
+                                                    style={
+                                                        ({ isActive }) => isActive ? { "border-bottom": "solid red 3px" } : {}
+                                                    }
+                                                    to="/contact-us">
+                                                    Contact Us
+                                                </NavLink>
+                                            </li>
+                                        </ul>
+                                        <div className="user_option">
+                                            <form className="form-inline my-2 my-lg-0 ml-0 ml-lg-4 mb-3 mb-lg-0">
+                                                <button
+                                                    className="btn  my-2 my-sm-0 nav_search-btn"
+                                                    type="submit"
+                                                ></button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </nav>
+                        </div>
+                    </header>
+
+                </div>
+            </header>
+
+        </>
+
+
+
+    )
+}
