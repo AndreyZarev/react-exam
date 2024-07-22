23326e33b8a1357f383971853eeef052ad70ad85
tocode <andrey.zarev@outlook.com>
2024-07-22 18:04:22 +0300

    Create Section-footer.jsx

---
+++ client/vite-project/src/components/zfooter/Section-footer.jsx
@@ -0,0 +1,11 @@
+export default function SectionFooter() {
+    return (
+        <footer className="container-fluid footer_section">
+            <p>
+                © 2020 All Rights Reserved. Design by
+                <a href="https://html.design/">Free Html Templates</a>
+            </p>
+        </footer>
+
+    )
+}
