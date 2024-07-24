eaa51a7ed872d2b815e443259cc73d95ae276fc1
tocode <andrey.zarev@outlook.com>
2024-07-24 17:46:59 +0300

    Update Trainers.jsx

--- client/vite-project/src/components/main/section-trainer/trainers/Trainers.jsx
+++ client/vite-project/src/components/main/section-trainer/trainers/Trainers.jsx
@@ -1,33 +1,34 @@
 import { Link } from "react-router-dom"
 
+import "../trainer-details/trainers.css"
 export default function Trainers({
+    img,
     _id,
     person,
     phone
 }
 ) {
-
     return (
 
         <div className="row">
-            <div className="col-lg-4 col-md-6 mx-auto">
-                <div className="box">
-                    <div className="name">
-                        <h5>{person}</h5>
-                    </div>
-                    <div className="img-box">
-                        <img src="images/t1.jpg" alt="" />
-                    </div>
-                    <div className="social_box">
-                        <p>{phone}</p>
-                    </div>
-                    <button>
 
-                        <Link to="/trainers/details/trainerid" >
-                            Details
-                        </Link>                    </button>
+            <div className="box">
+                <div className="name">
+                    <h5>{person}</h5>
                 </div>
+                <div className="img-box">
+                    <img src={img} alt="" />
+                </div>
+                <div className="social_box">
+                    <p>{phone}</p>
+                </div>
+                <button>
+
+                    <Link to={`/trainers/${_id}`} >
+                        Details
+                    </Link>                    </button>
             </div>
+
 
 
         </div>
