050a848283e93a306c95b441d6112f33e4be0bc1
tocode <andrey.zarev@outlook.com>
2024-07-24 17:46:49 +0300

    Update Home.jsx

--- client/vite-project/src/components/main/home/Home.jsx
+++ client/vite-project/src/components/main/home/Home.jsx
@@ -10,27 +10,6 @@ export default function Home() {
     return (
         <>
 
-            <meta charSet="utf-8" />
-            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1, shrink-to-fit=no"
-            />
-            <meta name="keywords" content="" />
-            <meta name="description" content="" />
-            <meta name="author" content="" />
-            <title>Neogym</title>
-            <link
-                rel="stylesheet"
-                type="text/css"
-                href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
-            />
-            <link
-                href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap"
-                rel="stylesheet"
-            />
-
-
             <body className="body">
 
 
