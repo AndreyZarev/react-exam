cfce460718f445fcb25eef8a484342c210de8cb7
tocode <andrey.zarev@outlook.com>
2024-07-25 22:03:28 +0300

    Update Section-us.jsx

--- client/vite-project/src/components/main/section-us/Section-us.jsx
+++ client/vite-project/src/components/main/section-us/Section-us.jsx
@@ -1,72 +1,58 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+import * as api from "../../api/api-links"
+
+
 export default function SectionUs() {
+    const [request, setRequest] = useState([])
+    const base = api.baseUrl
+
+    useEffect(() => {
+        (async () => {
+            const getRequest = await fetch(`${base}/why-us`)
+            const responseJson = await getRequest.json()
+            console.log(responseJson)
+
+            setRequest((Object.values(responseJson)))
+        })()
+    }, [])
+
+    console.log(request)
+
+
     return (
-        <section className="us_section layout_padding">
+        < section className="us_section layout_padding" >
             <div className="container">
                 <div className="heading_container">
                     <h2>Why Choose Us</h2>
                 </div>
                 <div className="us_container ">
-                    <div className="row">
-                        <div className="col-lg-3 col-md-6">
-                            <div className="box">
-                                <div className="img-box">
-                                    <img src="images/u-1.png" alt="" />
-                                </div>
-                                <div className="detail-box">
-                                    <h5>QUALITY EQUIPMENT</h5>
-                                    <p>
-                                        ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                        eiusmod tempor
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-md-6">
-                            <div className="box">
-                                <div className="img-box">
-                                    <img src="images/u-4.png" alt="" />
-                                </div>
-                                <div className="detail-box">
-                                    <h5>NUTRITION</h5>
-                                    <p>
-                                        ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                        eiusmod tempor
-                                    </p>
+
+                    {request.map(data => {
+
+                        <div className="row">
+                            <div className="col-lg-3 col-md-6">
+                                <div className="box">
+                                    <div className="img-box">
+                                        <img src={data.img} alt="" />
+                                    </div>
+                                    <div className="detail-box">
+                                        <h5>{data.title}</h5>
+                                        <p>
+                                            {data.text}
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-3 col-md-6">
-                            <div className="box">
-                                <div className="img-box">
-                                    <img src="images/u-2.png" alt="" />
-                                </div>
-                                <div className="detail-box">
-                                    <h5>HEALTHY DIET PLAN</h5>
-                                    <p>
-                                        ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                        eiusmod tempor
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-md-6">
-                            <div className="box">
-                                <div className="img-box">
-                                    <img src="images/u-3.png" alt="" />
-                                </div>
-                                <div className="detail-box">
-                                    <h5>SPORT TRAINING</h5>
-                                    <p>
-                                        ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                        eiusmod tempor
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
+                    })}
+
+
                 </div>
             </div>
-        </section>
+        </section >
 
     )
 }
