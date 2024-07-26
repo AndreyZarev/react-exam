2a969e26cdb4af6d6a4c44b09d78f480c98a3d46
tocode <andrey.zarev@outlook.com>
2024-07-27 00:52:47 +0300

    Update Section-us.jsx

--- client/vite-project/src/components/main/section-us/Section-us.jsx
+++ client/vite-project/src/components/main/section-us/Section-us.jsx
@@ -2,7 +2,8 @@ import { useEffect } from "react"
 import { useState } from "react"
 
 import * as api from "../../api/api-links"
-
+import SectionUsArticle from "./Section-us-article"
+import styles from "./section-us-article.module.css"
 
 export default function SectionUs() {
     const [request, setRequest] = useState([])
@@ -12,45 +13,28 @@ export default function SectionUs() {
         (async () => {
             const getRequest = await fetch(`${base}/why-us`)
             const responseJson = await getRequest.json()
-            console.log(responseJson)
-
-            setRequest((Object.values(responseJson)))
+            const result = Object.values(responseJson)
+            setRequest(result)
         })()
     }, [])
 
-    console.log(request)
 
 
     return (
-        < section className="us_section layout_padding" >
-            <div className="container">
-                <div className="heading_container">
-                    <h2>Why Choose Us</h2>
-                </div>
-                <div className="us_container ">
+        < section className={styles.section} >
 
-                    {request.map(data => {
+            <div className="heading_container">
+                <h2>Why Choose Us</h2>
+            </div>
+            <div className={styles.us_container}>
 
-                        <div className="row">
-                            <div className="col-lg-3 col-md-6">
-                                <div className="box">
-                                    <div className="img-box">
-                                        <img src={data.img} alt="" />
-                                    </div>
-                                    <div className="detail-box">
-                                        <h5>{data.title}</h5>
-                                        <p>
-                                            {data.text}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    })}
+                {request.map(data => <SectionUsArticle key={data._id} {...data} />
+                )}
 
 
-                </div>
+
+
+
             </div>
         </section >
 
