eb1fbf43bef79a316813224a0dac853159bd235d
tocode <andrey.zarev@outlook.com>
2024-08-02 17:29:52 +0300

    Update Header.jsx

--- client/vite-project/src/components/header/Header.jsx
+++ client/vite-project/src/components/header/Header.jsx
@@ -1,119 +1,116 @@
 
 import { NavLink, Link } from "react-router-dom"
-const baseUrl = "http://localhost:3030/jsonstore"
+import { useContext } from "react"
+
+import { AuthContext } from "../contexts/Auth-context"
+import "./header.css"
+
+
+
+
+
+
 
 export default function Header() {
-
+    const { isAuthenticated } = useContext(AuthContext)
     return (
         <>
 
-            <meta charSet="utf-8" />
-            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1, shrink-to-fit=no"
-            />
-            <meta name="keywords" content="" />
-            <meta name="description" content="" />
-            <meta name="author" content="" />
-            <title>Neogym</title>
-            <link
-                rel="stylesheet"
-                type="text/css"
-                href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
-            />
-            <link
-                href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap"
-                rel="stylesheet"
-            />
-
 
             <header>
-                <div className="hero_area">
-                    <header className="header_section">
-                        <div className="container-fluid">
-                            <nav className="navbar navbar-expand-lg custom_nav-container ">
-                                <Link to="/">
-                                    <span>Neogym</span>
-                                </Link>
-                                <button
-                                    className="navbar-toggler"
-                                    type="button"
-                                    data-toggle="collapse"
-                                    data-target="#navbarSupportedContent"
-                                    aria-controls="navbarSupportedContent"
-                                    aria-expanded="false"
-                                    aria-label="Toggle navigation"
-                                >
-                                    <span className="navbar-toggler-icon"></span>
-                                </button>
-                                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                    <div className="d-flex ml-auto flex-column flex-lg-row align-items-center">
-                                        <ul className="navbar-nav  ">
-                                            <li className="nav-item active">
+                <nav className="navbar navbar-expand-lg custom_nav-container ">
+                    <Link className="link" to="/">
+                        <span>Neogym</span>
+                    </Link>
 
-                                                <NavLink className="nav-link"
-                                                    style={
-                                                        ({ isActive }) => isActive ? { "borderBottom": "solid red 3px" } : {}
-                                                    }
-                                                    to="/">
-                                                    Home
-                                                </NavLink>
-                                            </li>
-                                            <li className="nav-item ">
-                                                <NavLink className="nav-link"
-                                                    style={
-                                                        ({ isActive }) => isActive ? { "borderBottom": "solid red 3px" } : {}
-                                                    }
-                                                    to="/why-us">
-                                                    Why Us
-                                                </NavLink>
-                                            </li>
-                                            <li className="nav-item">
-                                                <NavLink className="nav-link"
-                                                    style={
-                                                        ({ isActive }) => isActive ? { "borderBottom": "solid red 3px" } : {}
-                                                    }
-                                                    to="/trainers">
-                                                    Trainers
-                                                </NavLink>
-                                            </li>
-                                            <li className="nav-item">
-                                                <NavLink className="nav-link"
-                                                    style={
-                                                        ({ isActive }) => isActive ? { "borderBottom": "solid red 3px" } : {}
-                                                    }
-                                                    to="/register">
-                                                    Register
-                                                </NavLink>
-                                            </li>
-                                            <li className="nav-item">
-                                                <NavLink className="nav-link"
-                                                    style={
-                                                        ({ isActive }) => isActive ? { "borderBottom": "solid red 3px" } : {}
-                                                    }
-                                                    to="/login">
-                                                    Login
-                                                </NavLink>
-                                            </li>
-                                            <li className="nav-item">
-                                                <Link className="nav-link"
+                    <div className="navigation">
+                        <ul className="navbar-nav  ">
+                            <li className="nav-item home">
 
-                                                    to="/logout">
-                                                    Logout
-                                                </Link>
-                                            </li>
-                                        </ul>
+                                <NavLink className="nav-link "
+                                    style={
+                                        ({ isActive }) => isActive ? { "borderBottom": "solid red 3px" } : {}
+                                    }
+                                    to="/">
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className="nav-item why-us">
+                                <NavLink className="nav-link"
+                                    style={
+                                        ({ isActive }) => isActive ? { "borderBottom": "solid red 3px" } : {}
+                                    }
+                                    to="/why-us">
+                                    Why Us
+                                </NavLink>
+                            </li>
+                            <li className="nav-item trainers" >
+                                <NavLink className="nav-link"
+                                    style={
+                                        ({ isActive }) => isActive ? { "borderBottom": "solid red 3px" } : {}
+                                    }
+                                    to="/trainers">
+                                    Trainers
+                                </NavLink>
+                            </li>
+                            {isAuthenticated
+                                ? (
+                                    <>
+                                        <li className="nav-item create">
+                                            <NavLink className="nav-link"
+                                                style={
+                                                    ({ isActive }) => isActive ? { "borderBottom": "solid red 3px" } : {}
+                                                }
+                                                to="/create-offer">
+                                                Create Offer
+                                            </NavLink>
+                                        </li>
+                                        <li className="nav-item logout">
+                                            <Link className="nav-link"
 
-                                    </div>
-                                </div>
-                            </nav>
-                        </div>
-                    </header>
+                                                to="/logout">
+                                                Logout
+                                            </Link>
+                                        </li>
 
-                </div>
+
+
+                                    </>
+                                )
+                                : (
+                                    <>
+                                        <li className="nav-item login">
+                                            <NavLink className="nav-link"
+                                                style={
+                                                    ({ isActive }) => isActive ? { "borderBottom": "solid red 3px" } : {}
+                                                }
+                                                to="/login">
+                                                Login
+                                            </NavLink>
+                                        </li>
+                                        <li className="nav-item register">
+                                            <NavLink className="nav-link"
+                                                style={
+                                                    ({ isActive }) => isActive ? { "borderBottom": "solid red 3px" } : {}
+                                                }
+                                                to="/register">
+                                                Register
+                                            </NavLink>
+                                        </li>
+                                    </>
+                                )
+                            }
+
+
+
+                        </ul>
+
+                    </div>
+                </nav>
+
+
+
             </header>
-
         </>
 
 
