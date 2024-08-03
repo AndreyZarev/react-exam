b0b1357050e27693c1dd920ab1d7e3773f461c54
tocode <andrey.zarev@outlook.com>
2024-08-03 07:35:58 +0300

    Update Section-login.jsx

--- client/vite-project/src/components/main/sections-auth/Section-login.jsx
+++ client/vite-project/src/components/main/sections-auth/Section-login.jsx
@@ -4,25 +4,32 @@ import { useNavigate } from 'react-router-dom';
 
 import { useLogin } from '../../hooks/useAuth';
 import { useForm } from '../../hooks/useForm';
+import { useState } from 'react';
 
 
-
+const initialValues = { email: '', password: '' }
 
 export default function SectionLogin() {
+    const [error, setError] = useState("")
     const login = useLogin();
     const navigate = useNavigate();
-    const { values, changeHandler, submitHandler } = useForm(
-        { email: '', password: '' },
-        async (email, password) => {
-            try {
-                login(email, password)
-                navigate('/')
-            } catch (err) {
-                console.log(err.message);
-            }
-        }
 
-    )
+
+    const loginHandler = async (values) => {
+        try {
+            await login(values.email, values.password)
+            navigate('/')
+        } catch (err) {
+            setError(err.message);
+        }
+    }
+
+    const {
+        values,
+        changeHandler,
+        submitHandler
+    } = useForm(initialValues, loginHandler)
+
 
     return (
         <section className="contact_section ">
@@ -37,6 +44,7 @@ export default function SectionLogin() {
                         <div className="form_container pr-0 pr-lg-5 mr-0 mr-lg-2">
                             <div className="heading_container">
                                 <h2>Login</h2>
+                                <h4 className="errorMessage" style={{ color: "red" }}>{error}</h4>
                             </div>
                             <form className='login-form' onSubmit={submitHandler} >
 
