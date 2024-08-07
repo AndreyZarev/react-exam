3fec8555fd8ab17181750ee18bfc68841982c3b0
tocode <andrey.zarev@outlook.com>
2024-08-07 17:47:45 +0300

    Update SectionTrainer.jsx

--- client/vite-project/src/components/main/section-trainer/SectionTrainer.jsx
+++ client/vite-project/src/components/main/section-trainer/SectionTrainer.jsx
@@ -37,7 +37,7 @@ export default function SectionTrainer() {
                 )}
             </div>
 
-            <Link to="/register" className='link-register'>Register</Link>
+
         </section>
 
     )
