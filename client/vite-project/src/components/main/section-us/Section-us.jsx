32677ab26511fadb6464046aeefcaf6321a84a55
tocode <andrey.zarev@outlook.com>
2024-08-07 17:47:48 +0300

    Update Section-us.jsx

--- client/vite-project/src/components/main/section-us/Section-us.jsx
+++ client/vite-project/src/components/main/section-us/Section-us.jsx
@@ -3,18 +3,17 @@ import { useState } from "react"
 import { Link } from "react-router-dom"
 import { useContext } from "react"
 import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../../../contexts/Auth-context"
 
-
-
-import { AuthContext } from "../../contexts/Auth-context"
-import * as api from "../../api/api-links"
+import * as api from "../../../api/api-links"
 import SectionUsArticle from "./Section-us-article"
 import style from "./section-us.module.css"
 
 export default function SectionUs() {
     const [request, setRequest] = useState([])
-    const base = api.baseUrl
-    const { isAuthenticated } = useContext(AuthContext)
+    const dataUrl = api.dataUrl
+    const baseUrl = api.baseUrl
+    const isAuthenticated = useContext(AuthContext)
     const navigate = useNavigate()
 
     function register() {
@@ -24,12 +23,37 @@ export default function SectionUs() {
         navigate("/create-offer")
     }
     useEffect(() => {
-        (async () => {
-            const getRequest = await fetch(`${base}/why-us`)
-            const responseJson = await getRequest.json()
-            const result = Object.values(responseJson)
-            setRequest(result)
-        })()
+
+        const fetchData = async () => {
+            // try {
+            // Fetching both requests simultaneously
+            const [response1, response2] = await Promise.all([
+                fetch(`${baseUrl}/why-us`),
+                fetch(`${dataUrl}/why-us`),
+            ]);
+
+            if (response2.status !== 200) {
+                console.log(response2.status);
+                const data1 = await response1.json();
+                const result1 = Object.values(data1)
+                setRequest(result1)
+            } else {
+
+                // Parsing the JSON from the responses
+                const data1 = await response1.json();
+                const data2 = await response2.json();
+
+
+                const result1 = Object.values(data1)
+                const result2 = Object.values(data2)
+                const mergedArrays = [...result1, ...result2]
+                setRequest(mergedArrays)
+            }
+
+        };
+
+        fetchData();
+
     }, [])
 
 
