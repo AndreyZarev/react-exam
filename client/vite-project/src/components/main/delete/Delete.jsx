70613074ef555ba1c144322cecf47bd0014fd69e
tocode <andrey.zarev@outlook.com>
2024-08-08 22:11:09 +0300

    implemented delete component functionality

--- client/vite-project/src/components/main/delete/Delete.jsx
+++ client/vite-project/src/components/main/delete/Delete.jsx
@@ -1,23 +1,28 @@
-// import { useNavigate } from "react-router-dom";
-// import { useContext } from "react";
-// import { useParams } from "react-router-dom";
-// import { AuthContext } from "../../../contexts/Auth-context";
-// import { deleteP } from "../../../hooks/useFetch";
+import { useNavigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { AuthContext } from "../../../contexts/Auth-context";
+import { deleteP } from "../../../hooks/useFetch";
 
-// export default  function DeletePost() {
-//     const { postid } = useParams();
-//     const navigate = useNavigate()
-//     const headers = {
-//         'Content-Type': 'application/json',
-//     };
+export default function DeletePost() {
+    const { postid } = useParams();
+    const navigate = useNavigate()
+    const headers = {
+        'Content-Type': 'application/json',
+    };
 
-//     const { accessToken } = useContext(AuthContext)
+    const { accessToken } = useContext(AuthContext)
 
 
-//     if (accessToken) {
-//         headers['X-Authorization'] = accessToken;
-//     }
-// export default async function DeletePost
-//     await deleteP(headers, postid)
-//     navigate("/why-us")
-// }
+    if (accessToken) {
+        headers['X-Authorization'] = accessToken;
+    }
+    useEffect(() => {
+        (async function deleteAndNav() {
+            await deleteP(headers, postid)
+            navigate("/why-us")
+        })()
+
+    }, [postid])
+
+}
