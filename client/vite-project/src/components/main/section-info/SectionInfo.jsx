f889c7395167519b6fdfe44ad9969edf7499e0c8
tocode <andrey.zarev@outlook.com>
2024-08-09 19:28:41 +0300

    relocated SectionInfo

--- client/vite-project/src/components/main/section-info/SectionInfo.jsx
+++ client/vite-project/src/components/main/section-info/SectionInfo.jsx
@@ -1,42 +1,42 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import style from "./section-info.module.css";
 
 export default function SectionInfo() {
     return (
-        <section className="info_section layout_padding2">
-            <div className="container">
-                <div className="info_items">
-                    <a href="">
-                        <div className="item ">
-                            <div className="img-box box-1">
-                                <img src="" alt="" />
-                            </div>
-                            <div className="detail-box">
-                                <p>Location</p>
-                            </div>
-                        </div>
-                    </a>
-                    <a href="">
-                        <div className="item ">
-                            <div className="img-box box-2">
-                                <img src="" alt="" />
-                            </div>
-                            <div className="detail-box">
-                                <p>+02 1234567890</p>
-                            </div>
-                        </div>
-                    </a>
-                    <a href="">
-                        <div className="item ">
-                            <div className="img-box box-3">
-                                <img src="" alt="" />
-                            </div>
-                            <div className="detail-box">
-                                <p>demo@gmail.com</p>
-                            </div>
-                        </div>
-                    </a>
+
+        <div className={style.container}>
+
+
+            <div className="item ">
+                <div className="img-box box-1">
+                    <FontAwesomeIcon icon="fa-solid fa-location-dot" />
+
+                    <p className={style.paragraph}>Location</p>
                 </div>
             </div>
-        </section>
+
+
+            <div className="item ">
+                <div className="img-box box-2">
+                    <img src="" alt="" />
+                </div>
+                <div className="detail-box">
+                    <p className={style.paragraph}>+02 1234567890</p>
+                </div>
+            </div>
+
+            <div className="item ">
+                <div className="img-box box-3">
+                    <img src="" alt="" />
+                </div>
+                <div className="detail-box">
+                    <p className={style.paragraph}>demo@gmail.com</p>
+                </div>
+            </div>
+
+        </div>
+
+
 
     )
 }
