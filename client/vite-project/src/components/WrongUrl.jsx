86933a4f738b566e67c389a488200247dddfaacb
tocode <andrey.zarev@outlook.com>
2024-08-10 12:41:52 +0300

    Create WrongUrl.jsx

---
+++ client/vite-project/src/components/WrongUrl.jsx
@@ -0,0 +1,33 @@
+
+import { Link } from "react-router-dom"
+
+
+
+
+export default function WrongUrl() {
+
+    return (
+        <>
+            <section className="section">
+                <div>
+                    <img className="img" src="https://lh6.googleusercontent.com/Bu-pRqU_tWZV7O3rJ5nV1P6NjqFnnAs8kVLC5VGz_Kf7ws0nDUXoGTc7pP87tyUCfu8VyXi0YviIm7CxAISDr2lJSwWwXQxxz98qxVfMcKTJfLPqbcfhn-QEeOowjrlwX1LYDFJN" alt="" />
+                </div>
+
+                <div className="">
+                    <button>
+
+                        <Link to="/">Return to Home</Link>
+
+                    </button>
+
+                </div>
+
+
+
+
+
+            </section>
+
+        </>
+    )
+}
