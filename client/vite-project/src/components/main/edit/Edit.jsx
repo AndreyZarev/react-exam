50dd929d6fd424ba4b803e4f10d0645a7c2f94fc
tocode <andrey.zarev@outlook.com>
2024-08-10 07:41:16 +0300

    styled edit

--- client/vite-project/src/components/main/edit/Edit.jsx
+++ client/vite-project/src/components/main/edit/Edit.jsx
@@ -8,6 +8,7 @@ import * as api from "../../../api/api-links"
 import { useForm } from "../../../hooks/useForm"
 import { update } from "../../../hooks/useFetch";
 import { AuthContext } from "../../../contexts/Auth-context";
+import style from "./edit.module.css"
 
 export default function Edit() {
     const dataUrl = api.dataUrl;
@@ -60,59 +61,48 @@ export default function Edit() {
     }
 
     return (
-        <section className="contact_section">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-6 px-0">
-                        <div className="img-box">
-                            <img src="images/contact-img.jpg" alt="" />
-                        </div>
-                    </div>
-                    <div className="col-lg-5 col-md-6">
-                        <div className="form_container pr-0 pr-lg-5 mr-0 mr-lg-2">
-                            <div className="heading_container">
-                                <h2>Edit Post</h2>
-                                <h4 className="errorMessage" style={{ color: "red" }}>{error}</h4>
-                            </div>
+        <section className={style.section}>
 
-                            <form className='login-form' onSubmit={submitHandler}>
-                                <label htmlFor="img">Image:</label>
-                                <div>
-                                    <input
-                                        type="text"
-                                        name='img'
-                                        value={values.img || ""}
-                                        onChange={changeHandler}
-                                    />
-                                </div>
-                                <label htmlFor="title">Name:</label>
-                                <div>
-                                    <input
-                                        type="text"
-                                        name="title"
-                                        className="title"
-                                        value={values.title || ""}
-                                        onChange={changeHandler}
-                                    />
-                                </div>
-                                <label htmlFor="text">Text:</label>
-                                <div>
-                                    <input
-                                        type="text"
-                                        name="text"
-                                        className="text"
-                                        value={values.text || ""}
-                                        onChange={changeHandler}
-                                    />
-                                </div>
-                                <div className="d-flex">
-                                    <button type="submit">Send</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+            <div className="heading_container">
+                <h2 className={style.title}>Edit Post</h2>
+                <h4 className="errorMessage" style={{ color: "red" }}>{error}</h4>
             </div>
+
+            <form className={style.form} onSubmit={submitHandler}>
+                <label htmlFor="img">Image:</label>
+                <div>
+                    <input
+                        type="text"
+                        name='img'
+                        value={values.img || ""}
+                        onChange={changeHandler}
+                    />
+                </div>
+                <label htmlFor="title">Name:</label>
+                <div>
+                    <input
+                        type="text"
+                        name="title"
+                        className="title"
+                        value={values.title || ""}
+                        onChange={changeHandler}
+                    />
+                </div>
+                <label htmlFor="text">Text:</label>
+                <div>
+                    <input
+                        type="text"
+                        name="text"
+                        className="text"
+                        value={values.text || ""}
+                        onChange={changeHandler}
+                    />
+                </div>
+                <div className="d-flex">
+                    <button className={style.sendBtn} type="submit">Send</button>
+                </div>
+            </form>
+
         </section>
     );
 }
