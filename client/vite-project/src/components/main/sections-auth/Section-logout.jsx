d90452643bf52e9b51816a1b1d4a90bea93115b5
tocode <andrey.zarev@outlook.com>
2024-08-10 07:39:32 +0300

    added use effect to logout

---
+++ client/vite-project/src/components/main/sections-auth/Section-logout.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+
+import { AuthContext } from "../../../contexts/Auth-context";
+import { useLogout } from "../../../hooks/useAuth";
+
+export default function Logout() {
+    const navigate = useNavigate()
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    const { accessToken } = useContext(AuthContext)
+
+
+    if (accessToken) {
+        headers['X-Authorization'] = accessToken;
+    }
+    const logout = useLogout()
+    useEffect(() => {
+
+        (async (headers) => {
+            logout(headers)
+            navigate("/")
+        })()
+
+    }, [])
+
+
+
+}
