f8ece7fdcc655f2796fd3a57ff4bff4f7aed66ec
tocode <andrey.zarev@outlook.com>
2024-08-10 07:42:05 +0300

    styled section info

--- client/vite-project/src/components/main/section-info/SectionInfo.jsx
+++ client/vite-project/src/components/main/section-info/SectionInfo.jsx
@@ -1,4 +1,4 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import style from "./section-info.module.css";
 
 export default function SectionInfo() {
@@ -9,9 +9,9 @@ export default function SectionInfo() {
 
             <div className="item ">
                 <div className="img-box box-1">
-                    <FontAwesomeIcon icon="fa-solid fa-location-dot" />
 
-                    <p className={style.paragraph}>Location</p>
+
+                    <p className={style.paragraph}>Location: <span className={style.span}>3044 Shepherd Street, Branson, MO, Zip Code: 65616</span></p>
                 </div>
             </div>
 
@@ -21,7 +21,7 @@ export default function SectionInfo() {
                     <img src="" alt="" />
                 </div>
                 <div className="detail-box">
-                    <p className={style.paragraph}>+02 1234567890</p>
+                    <p className={style.paragraph}>Phone number: <span className={style.span}>+02 1234567890</span></p>
                 </div>
             </div>
 
@@ -30,7 +30,7 @@ export default function SectionInfo() {
                     <img src="" alt="" />
                 </div>
                 <div className="detail-box">
-                    <p className={style.paragraph}>demo@gmail.com</p>
+                    <p className={style.paragraph}>Email address: <span className={style.span}>demo@gmail.com</span></p>
                 </div>
             </div>
 
