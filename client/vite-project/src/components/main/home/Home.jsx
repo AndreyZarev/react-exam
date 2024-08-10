388b8a78e30b55cb8732576b566018fbf395a9b6
tocode <andrey.zarev@outlook.com>
2024-08-10 07:41:34 +0300

    Update Home.jsx

--- client/vite-project/src/components/main/home/Home.jsx
+++ client/vite-project/src/components/main/home/Home.jsx
@@ -1,180 +1,47 @@
 
+import { Link } from "react-router-dom"
 
-
-import { useState } from "react";
-import SectionInfo from "../section-info/SectionInfo";
-
+import style from "./home.module.css"
 
 
 export default function Home() {
 
     return (
         <>
-
-
-
-
-
-
-            <section className=" slider_section position-relative">
-                <div
-                    id="carouselExampleIndicators"
-                    className="carousel slide"
-                    data-ride="carousel"
-                >
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <div className="container">
-                                <div className="col-lg-10 col-md-11 mx-auto">
-                                    <div className="detail-box">
-                                        <div>
-                                            <h3>Fitness</h3>
-                                            <h2>Training</h2>
-                                            <h1>Neogym</h1>
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                                sed do eiusmod tempor incididunt ut labore et dolore magna
-                                                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                                Duis aute irure dolor in reprehenderit in voluptate velit
-                                                esse .
-                                            </p>
-                                            <div className="">
-                                                <a href="/contact-us">Contact Us</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <div className="container">
-                                <div className="col-lg-10 col-md-11 mx-auto">
-                                    <div className="detail-box">
-                                        <div>
-                                            <h3>Fitness</h3>
-                                            <h2>Training</h2>
-                                            <h1>Neogym</h1>
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                                sed do eiusmod tempor incididunt ut labore et dolore magna
-                                                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                                Duis aute irure dolor in reprehenderit in voluptate velit
-                                                esse .
-                                            </p>
-                                            <div className="">
-                                                <a href="/contact-us">Contact Us</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <div className="container">
-                                <div className="col-lg-10 col-md-11 mx-auto">
-                                    <div className="detail-box">
-                                        <div>
-                                            <h3>Fitness</h3>
-                                            <h2>Training</h2>
-                                            <h1>Neogym</h1>
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                                sed do eiusmod tempor incididunt ut labore et dolore magna
-                                                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                                Duis aute irure dolor in reprehenderit in voluptate velit
-                                                esse .
-                                            </p>
-                                            <div className="">
-                                                <a href="">Contact Us</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <div className="container">
-                                <div className="col-lg-10 col-md-11 mx-auto">
-                                    <div className="detail-box">
-                                        <div>
-                                            <h3>Fitness</h3>
-                                            <h2>Training</h2>
-                                            <h1>Neogym</h1>
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                                sed do eiusmod tempor incididunt ut labore et dolore magna
-                                                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                                Duis aute irure dolor in reprehenderit in voluptate velit
-                                                esse .
-                                            </p>
-                                            <div className="">
-                                                <a href="">Contact Us</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <div className="container">
-                                <div className="col-lg-10 col-md-11 mx-auto">
-                                    <div className="detail-box">
-                                        <div>
-                                            <h3>Fitness</h3>
-                                            <h2>Training</h2>
-                                            <h1>Neogym</h1>
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                                sed do eiusmod tempor incididunt ut labore et dolore magna
-                                                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                                Duis aute irure dolor in reprehenderit in voluptate velit
-                                                esse .
-                                            </p>
-                                            <div className="">
-                                                <a href="">Contact Us</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <ol className="carousel-indicators">
-                        <li
-                            data-target="#carouselExampleIndicators"
-                            data-slide-to="{0}"
-                            className="active"
-                        ></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="{1}"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="{2}"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="{3}"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="{4}"></li>
-                    </ol>
+            <section className={style.section}>
+                <div>
+                    <img className={style.img} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjC0G8utN-kFNn02UGU_Awv1tvTYwn0ipbmw&s" alt="" />
                 </div>
+                <div className={style.text}>
+
+                    <h3>Fitness</h3>
+                    <h2>Training</h2>
+                    <h1>Neogym</h1>
+                    <p className={style.textP}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse .
+                    </p>
+                    <div className="">
+                        <button>
+
+                            <Link to="/why-us">Check Posts</Link>
+
+                        </button>
+                        <button>
+                            <Link to="/trainers">Check Trainers</Link>
+
+                        </button>
+                    </div>
+
+
+                </div>
+
+
             </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         </>
     )
