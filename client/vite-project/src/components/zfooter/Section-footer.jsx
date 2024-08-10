0c87b1f7bd3d9cf6ed3b0973eed4bf7884b6443c
tocode <andrey.zarev@outlook.com>
2024-08-10 07:40:16 +0300

    styled footer

--- client/vite-project/src/components/zfooter/Section-footer.jsx
+++ client/vite-project/src/components/zfooter/Section-footer.jsx
@@ -1,9 +1,12 @@
+import SectionInfo from "../main/section-info/SectionInfo"
+import style from "./footer.module.css"
 export default function SectionFooter() {
     return (
-        <footer className="container-fluid footer_section">
-            <p>
-                © 2020 All Rights Reserved. Design by
-                <a href="https://html.design/">Free Html Templates</a>
+        <footer className={style.footer}>
+            <SectionInfo />
+
+            <p className={style.copyRight}>
+                © 2024 React Project All Rights Reserved.
             </p>
         </footer>
 
