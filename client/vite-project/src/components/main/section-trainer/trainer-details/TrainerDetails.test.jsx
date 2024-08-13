4e3d19171572f3bce306cccb2ce30aa25a0f4964
tocode <andrey.zarev@outlook.com>
2024-08-14 01:41:01 +0300

    added more unit tests to TrainerDetails

--- client/vite-project/src/components/main/section-trainer/trainer-details/TrainerDetails.test.jsx
+++ client/vite-project/src/components/main/section-trainer/trainer-details/TrainerDetails.test.jsx
@@ -1,9 +1,31 @@
 import { test, expect } from "vitest"
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom/vitest'
 
+import TrainerDetails from "./TrainerDetails"
+import { MemoryRouter } from 'react-router-dom';
 test("test true", () => {
     expect(true).toEqual(true);
 })
 test("test dom", () => {
-    console.log(document);
+    render(<TrainerDetails />)
+    const header = screen.getByText('One of our Gym Trainers')
 
+    expect(header).toBeVisible();
+
+})
+
+test("is dom there", () => {
+    // vi.mock('react-router-dom', () => ({
+    //     ...vi.importActual('react-router-dom'), // Import everything else from react-router-dom
+    //     useNavigate: () => vi.fn(), // Mock useNavigate as a jest mock function
+    // }));
+    render(<TrainerDetails />)
+
+    // <MemoryRouter>
+
+    // </MemoryRouter>
+    const header = screen.getByText('One of our Gym Trainers')
+
+    expect(header).toBeVisible();
 })
