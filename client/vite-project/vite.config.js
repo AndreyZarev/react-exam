dfb189fe8ebfa4315717403b8dbaba524ead7ffa
tocode <andrey.zarev@outlook.com>
2024-08-14 16:05:38 +0300

    fixed vite config to be able to import setup.js

--- client/vite-project/vite.config.js
+++ client/vite-project/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 
@@ -6,7 +6,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom'
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/tests-config/setup.js']
+
   },
-  setupFiles: './tests-config/setup.js'
 })
